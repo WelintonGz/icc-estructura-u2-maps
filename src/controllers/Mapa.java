96a9da2531270a7abb5220e5096f984ed0eab1ae
ArielPaltanC <146782047+ArielPaltanC@users.noreply.github.com>
2025-07-08 19:53:09 -0500

    un comit

--- src/controllers/Mapa.java
+++ src/controllers/Mapa.java
@@ -1,9 +1,96 @@
 package controllers;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import models.Empleado;
 
 public class Mapa {
     public Mapa() {
         System.out.println("Mapa Class");
+    }
+
+    public void runHashMap(){
+        System.out.println("----runHasMap----");
+        Map<String, String> mapa = new HashMap<>();
+        mapa.put("001", "Celular");
+        mapa.put("002", "Laptop");
+        mapa.put("003", "Celular");
+        mapa.put("003", "PC");
+
+        System.out.println(mapa);
+        System.out.println();
+        System.out.println(mapa.keySet());
+
+        for (String c : mapa.keySet()) {
+            System.out.println(mapa.get(c));
+        }
+        System.out.println(mapa.get("001"));
+        System.out.println(mapa.get("005"));
+    }
+
+    public void runLinkedHashMap(){
+        System.out.println("----runLinkedHasMap----");
+        Map<Integer, String> mapa2 = new LinkedHashMap<>();
+        mapa2.put(1, "Pedro");
+        mapa2.put(2, "Juan");
+        mapa2.put(3, "Marcelo");
+        mapa2.put(4, "Roger");
+        mapa2.put(5, "Milton");
+
+        System.out.println(mapa2);
+        System.out.println();
+        System.out.println(mapa2.keySet());
+
+        for (Integer c2 : mapa2.keySet()) {
+            System.out.println(mapa2.get(c2));
+        }
+
+        System.out.println(mapa2.get(1));
+        System.out.println(mapa2.get(6));
+    }
+
+    public void runTreeMap(){
+        System.out.println("----runTreeMap----");
+        Map<Integer, String> items = new TreeMap<>();
+        items.put(3, "Carro");
+        items.put(6, "bicicleta");
+        items.put(1, "moto");
+
+        System.out.println(items);
+        System.out.println();
+        System.out.println(items.keySet());
+    }
+    
+    public void runHashMapObj(){
+        System.out.println("----runHasMapObj----");
+        Map<Empleado, Integer> empleados = new HashMap();
+        empleados.put(new Empleado(1, "Juan", "Dev"),1200);
+        empleados.put(new Empleado(2, "Juan", "Dev"),1500);
+        empleados.put(new Empleado(3, "Pedro", "Dev"),2000);
+        empleados.put(new Empleado(1, "Juan", "Dev"),2500);
+        empleados.put(new Empleado(1, "Diego", "Dev"),3000);
+
+        System.out.println(empleados);
+        System.out.println();
+        System.out.println(empleados.keySet());
+
+    }
+
+    public void runTreeMapObj(){
+        System.out.println("----runTreeMapObj----");
+            Map<Empleado, Integer> empleados = new TreeMap<>();
+        empleados.put(new Empleado(1, "Juan", "Dev"),1200);
+        empleados.put(new Empleado(2, "Juan", "Dev"),1500);
+        empleados.put(new Empleado(3, "Pedro", "Dev"),2000);
+        empleados.put(new Empleado(1, "Diego", "Dev"),1200);
+        empleados.put(new Empleado(1, "Juan", "Senior"),3000);
+
+        System.out.println(empleados);
+        System.out.println();
+        System.out.println(empleados.keySet());
+
     }
 }
--- src/models/Empleado.java
+++ src/models/Empleado.java
@@ -1,6 +1,8 @@
 package models;
 
-public class Empleado {
+import java.util.Objects;
+
+public class Empleado implements Comparable<Empleado>{
     private int id;
     private String name;
     private String position;
@@ -9,6 +11,22 @@ public class Empleado {
         this.id = id;
         this.name = name;
         this.position = position;
+    }
+
+    @Override
+    public int hashCode(){
+        return id + name.hashCode();
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Empleado otraPersona = (Empleado) obj;
+        return Objects.equals(name, otraPersona.name) && id == otraPersona.id;
     }
 
     public int getId() {
@@ -26,5 +44,16 @@ public class Empleado {
     @Override
     public String toString() {
         return "ID: " + id + ", Name: " + name + ", Position: " + position;
+    }
+
+    @Override
+    public int compareTo(Empleado o) {
+        int result = Integer.compare(this.id, o.id);
+        if (result != 0) {
+            return result;
+        }
+        int nombre = this.name.compareTo(o.name);
+        return nombre;
+
     }
 }
